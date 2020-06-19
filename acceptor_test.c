b694757d4c04862f1c654324304ab51b16b4a082
Piotr Bosowski <piotr.bosowski@gmail.com>
2020-06-19 18:53:51 +0200

    Fix printf argument

--- acceptor_test.c
+++ acceptor_test.c
@@ -105,7 +105,7 @@ int wait_for(int pin_number, int state, int timeout_allowed)
 int pay(int nominal)
 {
   printf("Initializing payment of ");
-  printf("%s", nominalsValues[nominal]);
+  printf("%d", nominalsValues[nominal]);
   digitalWrite(INHIBIT_PINS[nominal], LOW); // allow corresponding channel
   int result = wait_for(ACCEPTED_PINS[nominal], LOW, 1); // "if a note is recognised, the relevant channel line is set LOW for 100 +- 3 milliseconds."
   digitalWrite(INHIBIT_PINS[nominal], HIGH);
